4f728d05658428cb7ec40c724e1276cd9f4deb7f
canblmz1 <22480099@stu.omu.edu.tr>
2025-07-06 15:44:49 +0300

    🚀 Add GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/e-ticaret-2040/',
+  build: {
+    outDir: 'dist',
+  }
 })
